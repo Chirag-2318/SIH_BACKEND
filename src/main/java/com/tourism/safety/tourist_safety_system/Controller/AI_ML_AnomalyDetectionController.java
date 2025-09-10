a95858d8f056a910569c7f5add7f2edd578df498
Chirag-2318 <chirag23nzr@gmail.com>
2025-09-10 16:03:55 +0530

    second commit

--- src/main/java/com/tourism/safety/tourist_safety_system/Controller/AI_ML_AnomalyDetectionController.java
+++ src/main/java/com/tourism/safety/tourist_safety_system/Controller/AI_ML_AnomalyDetectionController.java
@@ -1,7 +1,10 @@
 package com.tourism.safety.tourist_safety_system.Controller;
 
 public class AI_ML_AnomalyDetectionController {
-    for(int i=0;i<10;i++){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("Hello World");
+            System.out.println("Hello World");
+        }
     }
 }
